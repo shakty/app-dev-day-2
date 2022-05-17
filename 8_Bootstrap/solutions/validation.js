7bfe4cf6e2046e16f5168716dc9ec09491f339df
Stefano Balietti <futur.dorko@gmail.com>
2022-05-17 21:53:21 +0200

    validation

--- 8_Bootstrap/solutions/validation.js
+++ 8_Bootstrap/solutions/validation.js
@@ -12,31 +12,46 @@ const PWD_VALIDATE = text => {
 
 const NOT_EMPTY_VALIDATE = text => text.trim() !== "";
 
+// US ZIP code format.
 const ZIP_VALIDATE = text => /^\d{5}(-\d{4})?$/.test(text);
+
+const errMsgs = {
+    
+    abc: 'Cannot contain numbers',
+
+    email: 'Email not valid',
+
+    pwd: 'Must be at least 8 chars and contain a number or a special char',
+
+    noEmpty: 'Cannot be empty',
+
+    zip: 'Zip-code not valid'
+};
 
 function validateAndSetClass(form) {
 
     let id = form.id;
+    let cb;
 
-    if (id === "first" || id === "last") {
-        testInputAndSetClass(form, ABC_VALIDATE);
+    if (id === "first" || id === "last" || id === "city") {
+        cb = ABC_VALIDATE;
     }
     if (id === "email") {
-        testInputAndSetClass(form, EMAIL_VALIDATE);
+        cb = EMAIL_VALIDATE;
     }
     else if (id === "pwd") {
-      testInputAndSetClass(form, PWD_VALIDATE);
+        cb = PWD_VALIDATE;
     }
     else if (id === "address") {
-      testInputAndSetClass(form, NOT_EMPTY_VALIDATE);
-    }
-    else if (id === "city") {
-        testInputAndSetClass(form, ABC_VALIDATE);
+        cb = NOT_EMPTY_VALIDATE;
     }
     else if (id === "zip") {
-      testInputAndSetClass(form, ZIP_VALIDATE);
+        cb = ZIP_VALIDATE;
     }
 
+    console.log(id, cb)
+
+    testInputAndSetClass(form, cb);
 }
 
 function testInputAndSetClass(input, validateCb) {
@@ -61,9 +76,38 @@ function addEventListenerToInput(input) {
     });
 }
 
+function showErrMsg(form) {
+    let id = form.id;
+    let err;
+
+    if (id === "first" || id === "last" || id === "city") {
+        err = errMsgs.abc;
+    }
+    if (id === "email") {
+        err = errMsgs.email;
+    }
+    else if (id === "pwd") {
+        err = errMsgs.pwd;
+    }
+    else if (id === "address") {
+        err = errMsgs.noEmpty;
+    }
+    else if (id === "zip") {
+        err = errMsgs.zip;
+    }
+
+    let div = document.createElement('div');
+    div.className = 'invalid-feedback';
+    div.innerHTML = err;
+    form.parentElement.appendChild(div);
+}
+
+function hideErrMsg(form) {
+    // form.parentElement.remove
+    // TODO.
+}
 
 (function () {
-    'use strict'
 
     // Fetch all the forms that need validation.
     let forms = document.querySelectorAll('.form-control');
@@ -83,15 +127,17 @@ function addEventListenerToInput(input) {
 
         forms.forEach(function (form) {
 
-            //   validateAndSetClass(form[i]);
-            validateAndSetClass(form);
-
+            if (form.classList.contains('needs-validation')) {
+                validateAndSetClass(form);  
+            }
 
             if (form.classList.contains('is-invalid')) {
                 valid = false;
+
+                showErrMsg(form);
+
                 return;
             }
-
 
             data[form.id] = form.value;
 
