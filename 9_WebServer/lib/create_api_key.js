d8ee976fbdf7c12f93c75a567004327e3215d8d8
Stefano Balietti <futur.dorko@gmail.com>
2022-05-19 20:07:57 +0200

    minor

--- 9_WebServer/lib/create_api_key.js
+++ 9_WebServer/lib/create_api_key.js
@@ -1,6 +1,8 @@
 // Example adapted from:
 // https://www.syedhussim.com/node.js/generating-web-api-keys.html
 
+
+// First, create the API key.
 const SIZE = 40;
 const crypto = require("crypto");
 const rand = crypto.randomBytes(SIZE);
@@ -16,8 +18,10 @@ for (let i=0; i < rand.length; i++){
     key += chars[index];
 }
 
+// This is your API key.
 console.log(key);
 
+// Now, store it securely in hashed form with bcrypt.
 const path = require('path');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
@@ -25,7 +29,7 @@ const saltRounds = 10;
 
 bcrypt.hash(key, saltRounds, function(err, hash) {
     // Store hash to file system (could also be a database).
-
+    console.log(hash);
     let fileName = path.resolve(__dirname, '..', 'secret', 'key.js');
     fs.writeFileSync(fileName, `module.exports = "${hash}"`);
     console.log("Hash of API key created: " + "secret/key.js");
