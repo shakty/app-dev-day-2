d151a5298deb0d688e6833c1da184abebcb3f8ff
Stefano Balietti <futur.dorko@gmail.com>
2022-05-19 09:07:08 +0200

    modal fix

--- 9_WebServer/public/js/modal.js
+++ 9_WebServer/public/js/modal.js
@@ -1,14 +1,20 @@
 
 function doSolution() {
+    
+    let myModal;
+    
     // 1sec after hoovering over a footer we show the modal
     // of the survey.
     $("div.card-footer").hover(function () {
         setTimeout(() => {
 
+            // It may fire multiple times before it is unbinded.
+            if (myModal) return;
+
             // Create a new Modal object from our HTML template.
             // Notice: it would be better to create the myModal object 
             // outside of the listener and re-use it.
-            let myModal = new bootstrap.Modal(
+            myModal = new bootstrap.Modal(
                 document.getElementById('surveyModal')
             );
 
