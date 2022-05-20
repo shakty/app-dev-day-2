249175caf3d8fd95a3a6e63512625ccfb4356b28
Stefano Balietti <futur.dorko@gmail.com>
2022-05-20 14:54:55 +0200

    fixed

--- 10_MongoDB/public/js/activities.js
+++ 10_MongoDB/public/js/activities.js
@@ -41,23 +41,28 @@ function updateProgress(x) {
 }
 
 function doSolution() {
-    // 1sec after hoovering over a footer we show the modal
-    // of the survey.
-    $("div.card-footer").hover(function () {
-        setTimeout(() => {
+    
+  let myModal;
+  
+  // 1sec after hoovering over a footer we show the modal
+  // of the survey.
+  $("div.card-footer").hover(function () {
+      setTimeout(() => {
 
-            // Create a new Modal object from our HTML template.
-            // Notice: it would be better to create the myModal object 
-            // outside of the listener and re-use it.
-            let myModal = new bootstrap.Modal(
-                document.getElementById('surveyModal')
-            );
+          // It may fire multiple times before it is unbinded.
+          if (myModal) return;
 
-            myModal.show();
-        }, 1000);
-    });
+          // Create a new Modal object from our HTML template.
+          // Notice: it would be better to create the myModal object 
+          // outside of the listener and re-use it.
+          myModal = new bootstrap.Modal(
+              document.getElementById('surveyModal')
+          );
+
+          myModal.show();
+      }, 1000);
+  });
 }
-
 
 // Do something about it.
 function doSomethingAboutIt() {
